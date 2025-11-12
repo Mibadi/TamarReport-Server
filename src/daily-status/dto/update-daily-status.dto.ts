20e342945fce04f0755e11e00a3ad809a2afc251
mktnx72-ai <mktnx72@gmail.com>
2025-11-12 13:52:26 +0200

    API To Base

--- src/daily-status/dto/update-daily-status.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateDailyStatusDto } from './create-daily-status.dto';
-
-export class UpdateDailyStatusDto extends PartialType(CreateDailyStatusDto) {}
-
--- src/fault/dto/update-fault.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateFaultDto } from './create-fault.dto';
-
-export class UpdateFaultDto extends PartialType(CreateFaultDto) {}
